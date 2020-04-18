8f6224ebc1f546d8336f1d012be023182e2caa80
Gopalji Gaur <gopaljigaur@gmail.com>
2020-04-18 16:25:48 +0530

    Updated for popular faces

--- application.py
+++ application.py
@@ -12,6 +12,7 @@ from flask import Flask, Response, jsonify, request
 from PIL import Image, ImageFont, ImageDraw
 from titlecase import titlecase
 
+
 def analyze_img(rdata):
     #receive base64 encoded string and convert it to binary
     decoded_data = base64.b64decode(rdata)
@@ -34,14 +35,16 @@ def analyze_img(rdata):
     resp = requests.request("POST", url, data=payload, headers=headers, params=querystring)
     
     loaded_json = json.loads(resp.text)
-    
+
     faces = loaded_json['faces']
     num_face = len(faces)
     desc = loaded_json['description']
+    cel = loaded_json['categories'][0]['detail']['celebrities']
+    num_cel = len(cel)
     img_title = titlecase(desc['captions'][0]['text'])
-
+    
     tags = ', '.join([i for i in desc['tags'][0:8]])
-
+    
     x1=np.zeros(num_face)
     y1=np.zeros(num_face)
     x2=np.zeros(num_face)
@@ -56,31 +59,41 @@ def analyze_img(rdata):
         y2[i] = faces[i]['faceRectangle']['height'] + y1[i]
         age[i] = faces[i]['age']
         gender.append(faces[i]['gender'])
+    
+
+    x_c = np.zeros(num_cel)
+    y_c = np.zeros(num_cel)
+    for i in range(0,num_cel):
+        x_c[i] = cel[i]['faceRectangle']['left']
+        y_c[i] = cel[i]['faceRectangle']['top']
+        if(x_c[i] in x1 and y_c[i] in y1):
+            if(np.where(x1==x_c[i])==np.where(y1==y_c[i])):
+                gender[i] = cel[i]['name']
         
     #drawing and naming face rectangles
     for i in range(0,num_face):
         cv.rectangle(image,(int(x1[i]),int(y1[i])),(int(x2[i]),int(y2[i])),(255,255,255),2)
         #cv.putText(image, (gender[i] +" "+ str(int(age[i]))) , (int(x1[i]), int(y1[i])-5), cv.FONT_HERSHEY_SIMPLEX, 0.5,(255,255,255), 1)
     
-    fontpath = "./Fonts/Roboto-Medium.ttf"     
-    font = ImageFont.truetype(fontpath, 22)
+    fontpath = "./Roboto-Medium.ttf"     
+    font = ImageFont.truetype(fontpath, 16)
     
     img_pil = Image.fromarray(image)
     draw = ImageDraw.Draw(img_pil)
     for i in range(0,num_face):
         if (int(y1[i])-25)>0:
-            box_area = image[int(y1[i])-25:int(y1[i]),int(x1[i]):int(x2[i])]
-            textLoc = (int(x1[i]), int(y1[i])-25)
-            shadowLoc = (int(x1[i])+2, int(y1[i])-23)
+            box_area = image[int(y1[i])-18:int(y1[i]),int(x1[i]):int(x2[i])]
+            textLoc = (int(x1[i]), int(y1[i])-18)
+            shadowLoc = (int(x1[i])+1, int(y1[i])-17)
         elif (int(y1[i]))>0:
             box_area = image[0:int(y1[i]),int(x1[i]):int(x2[i])]
-            textLoc = (int(x1[i]), int(y1[i])-25)
-            shadowLoc = (int(x1[i])+2, int(y1[i])-23)
+            textLoc = (int(x1[i]), int(y1[i])-18)
+            shadowLoc = (int(x1[i])+1, int(y1[i])-17)
             
         elif (int(y1[i]))==0:
-            box_area = image[int(y2[i]):int(y2[i])+25,int(x1[i]):int(x2[i])]
-            textLoc = (int(x1[i]), int(y2[i])+5)
-            shadowLoc = (int(x1[i])+2, int(y2[i])+7)
+            box_area = image[int(y2[i]):int(y2[i])+20,int(x1[i]):int(x2[i])]
+            textLoc = (int(x1[i]), int(y2[i])+4)
+            shadowLoc = (int(x1[i])+1, int(y2[i])+5)
         
             
         box_area = cv.cvtColor(box_area,cv.COLOR_BGR2GRAY)
@@ -108,7 +121,6 @@ application = Flask(__name__)
 @application.route('/test',methods=['POST'])
 def test():
     r = request
-
     try:
         img_base64,img_title,tags = analyze_img(r.data)
         return jsonify({'Image':str(img_base64),'Image_Title':img_title,'Image_Tags':tags})
@@ -119,6 +131,7 @@ def main():
     return({'status':'Working'})
 
 # start flask app
+
 if __name__ == "__main__":
     application.run()
 
